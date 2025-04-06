571f8827b62a454676923e057ba981d5fee85223
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2025-04-06 17:06:39 +0100

    turn on line numbers

--- lua/nick/core/options.lua
+++ lua/nick/core/options.lua
@@ -3,27 +3,27 @@
 -----------------
 vim.cmd("let g:netrw_liststyle = 3") -- set exploreer style to tree
 
-local opt = vim.opt -- set local var for vim opt
+local opt = vim.opt                  -- set local var for vim opt
 
-opt.relativenumber = false -- set line numbers
-opt.number = false
+opt.relativenumber = true            -- set line numbers
+opt.number = true
 
 opt.wrap = false
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 stpaces for tabs
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab spaces
+opt.tabstop = 2       -- 2 stpaces for tabs
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search. assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search. assumes you want case-sensitive
 
 -- turn on termguicolors for certain colorschemes to work
 opt.termguicolors = true
 opt.background = "dark" -- sets colorschemes to dark mode
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard
