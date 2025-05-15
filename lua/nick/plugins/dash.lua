ac1ab938c892b3d10cf47a4912536318cb1aebf9
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2025-05-15 14:18:20 +0100

    remove unused config function for dash plugin

--- lua/nick/plugins/dash.lua
+++ lua/nick/plugins/dash.lua
@@ -4,14 +4,4 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     build = "make install",
-    config = function()
-        -- Wrap in pcall to prevent errors if module still isn't available
-        local ok, dash = pcall(require, "dash")
-        if not ok then
-            vim.notify(
-                "dash.nvim not properly installed. Try running 'cd ~/.local/share/nvim/lazy/dash.nvim && make install' manually",
-                vim.log.levels.WARN)
-            return
-        end
-    end,
 }
