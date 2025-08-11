8963fb651b4fb72383583948aa825110035101fa
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2025-08-11 23:05:05 +0100

    disable neocodium at startup

--- lua/nick/plugins/neocodium.lua
+++ lua/nick/plugins/neocodium.lua
@@ -3,7 +3,7 @@ return {
         event = "VeryLazy",
         config = function()
                 local neocodeium = require("neocodeium")
-                neocodeium.setup()
+                neocodeium.setup({enabled = false})
                 vim.keymap.set("i", "<A-tab>", neocodeium.accept)
                 vim.keymap.set("i", "<C-l>", neocodeium.accept_line)
                 vim.keymap.set("i", "<C-w>", neocodeium.accept_word)
