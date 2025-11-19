63409a8a33c0630faf9b89f6d322895470adbdd5
OwlfaceGames <108976740+OwlfaceGames@users.noreply.github.com>
2025-11-19 23:52:27 +0000

    temporarily disable indent guides

--- lua/nick/plugins/indent.lua
+++ lua/nick/plugins/indent.lua
@@ -1,13 +1,13 @@
 return {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {
-                scope = {
-                        enabled = true,
-                        show_start = false,
-                        show_end = false,
-                }
-        },
+--         "lukas-reineke/indent-blankline.nvim",
+--         main = "ibl",
+--         ---@module "ibl"
+--         ---@type ibl.config
+--         opts = {
+--                 scope = {
+--                         enabled = true,
+--                         show_start = false,
+--                         show_end = false,
+--                 }
+--         },
 }
