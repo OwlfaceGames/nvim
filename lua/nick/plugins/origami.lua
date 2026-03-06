6d377af174bc6f3f5c2c012bb88b391e5d0742e9
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2026-03-06 00:39:08 +0000

    remove fold keymaps from origami

--- lua/nick/plugins/origami.lua
+++ lua/nick/plugins/origami.lua
@@ -1,11 +1,15 @@
 return {
     "chrisgrieser/nvim-origami",
     event = "VeryLazy",
-    opts = {}, -- needed even when using default config
-
-    -- recommended: disable vim's auto-folding
     init = function()
         vim.opt.foldlevel = 99
         vim.opt.foldlevelstart = 99
+    end,
+    opts = {
+        hOnlyOpensOneLevel = false,
+    },
+    config = function(_, opts)
+        require("origami").setup(opts)
+        vim.keymap.del("n", "h")
     end,
 }
