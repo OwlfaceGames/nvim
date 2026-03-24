ef4c5646ca230d564b0de335b22247960215ac8c
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2026-03-24 17:52:42 +0000

    make control d and u start centered

--- lua/nick/core/keymaps.lua
+++ lua/nick/core/keymaps.lua
@@ -110,3 +110,7 @@ keymap.set("n", "<leader>uf", ":UndotreeFocus<CR>", {desc = "focus undo tree"})
 -- AMAZING move keybinds!!!
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- make control d and u stay centered
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
