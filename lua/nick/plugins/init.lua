1f496bad4e8ba48c400b3e84876ee71bfe3236be
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2026-03-25 14:42:58 +0000

    add no neck pain config

--- lua/nick/plugins/init.lua
+++ lua/nick/plugins/init.lua
@@ -2,7 +2,6 @@ return {
     "nvim-lua/plenary.nvim",
     "christoomey/vim-tmux-navigator",
     "nvim-treesitter/nvim-treesitter",
-    "shortcuts/no-neck-pain.nvim",
     "norcalli/nvim-colorizer.lua",
     "mbbill/undotree",
     "OwlfaceGames/owly.nvim",
