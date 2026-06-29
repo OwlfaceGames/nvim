aa564a42284e2f81ea928f0f1d8ad374f19c33ba
OwlfaceGames <owlfacegames@gmail.com>
2026-06-29 14:03:04 +0100

    remove lsp ass auto complete source

--- lua/nick/plugins/blink-cmp.lua
+++ lua/nick/plugins/blink-cmp.lua
@@ -40,7 +40,7 @@ return {
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'path', 'snippets', 'buffer' },
             -- default = { 'path', 'snippets' }, -- non intrusive
             providers = {
                 path = {
